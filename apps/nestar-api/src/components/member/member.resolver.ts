2794951cb05e00e412f6e3dc4574e74368d11853
Joe-jolly <sardorbek.rakhmonov@gmail.com>
2025-03-16 17:59:47 +0900

    feat: create checkAuthRoles for testing

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -1,11 +1,14 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { MemberService } from './member.service';
-import { InternalServerErrorException, UseGuards } from '@nestjs/common';
+import { ConsoleLogger, InternalServerErrorException, UseGuards } from '@nestjs/common';
 import { LoginInput, MemberInput } from '../../libs/dto/member/member.input';
 import { Member } from '../../libs/dto/member/member';
 import { AuthGuard } from '../auth/guards/auth.guard';
 import { AuthMember } from '../auth/decorators/authMember.decorator';
 import { ObjectId } from 'mongoose';
+import { Roles } from '../auth/decorators/roles.decorator';
+import { MemberType } from '../../libs/enums/member.enum';
+import { RolesGuard } from '../auth/guards/roles.guard';
 
 @Resolver()
 export class MemberResolver {
@@ -39,6 +42,14 @@ export class MemberResolver {
         return `Hi ${memberNick}`;
     }
 
+    @Roles(MemberType.USER, MemberType.AGENT)
+    @UseGuards(RolesGuard)
+    @Query(() => String)
+    public async checkAuthRoles(@AuthMember() authMember: Member): Promise<string> {
+        console.log("Query: checkAuth");
+        return `Hi ${authMember.memberNick}, you are ${authMember.memberType}, your memberId: ${authMember._id}`;
+    }
+
     @Query(() => String)
     public async getMember(): Promise<string> {
         console.log('Query: getMember');
@@ -47,10 +58,11 @@ export class MemberResolver {
 
     /** ADMIN **/
 
-    // Authorization: ADMIN
+    @Roles(MemberType.ADMIN)
+    @UseGuards(RolesGuard)
     @Mutation(() => String)
     public async getAllMembersByAdmin(): Promise<string> {
-        return this.getAllMembersByAdmin();
+        return this.memberService.getAllMembersByAdmin();
     }
 
     // Authorization: ADMIN
