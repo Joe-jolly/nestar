de556f64ca0e0ee4c2dc39eb7a7ad7cb2e888497
Joe-jolly <sardorbek.rakhmonov@gmail.com>
2025-03-30 19:02:46 +0900

    feat: develop removeCommentByAdmin GraphQL API

--- apps/nestar-api/src/components/comment/comment.service.ts
+++ apps/nestar-api/src/components/comment/comment.service.ts
@@ -104,5 +104,13 @@ export class CommentService {
 		if (!result.length) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
 
 		return result[0];
+    }
+    
+    /** ADMIN **/
+
+	public async removeCommentByAdmin(commentId: ObjectId): Promise<Comment> {
+		const result = await this.commentModel.findByIdAndDelete(commentId);
+		if (!result) throw new InternalServerErrorException(Message.REMOVE_FAILED);
+		return result;
 	}
 }
