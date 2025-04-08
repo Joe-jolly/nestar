07a3d35c9885e6932dcaa6052762da0b8653782f
Joe-jolly <sardorbek.rakhmonov@gmail.com>
2025-04-08 20:45:07 +0900

    fix: modify job scheduler business logic

--- apps/nestar-batch/src/batch.controller.ts
+++ apps/nestar-batch/src/batch.controller.ts
@@ -14,7 +14,7 @@ export class BatchController {
 		this.logger.debug('BATCH SERVER READY!');
   }
   
-  @Cron('00 * * * * *', { name: BATCH_ROLLBACK })
+  @Cron('00 00 01 * * *', { name: BATCH_ROLLBACK })
 	public async batchRollback() {
 		try {
 			this.logger['context'] = BATCH_ROLLBACK;
@@ -25,17 +25,17 @@ export class BatchController {
 		}
 	}
 
-	@Cron('20 * * * * *', { name: BATCH_TOP_PROPERTIES })
-	public async batchProperties() {
+	@Cron('20 00 01 * * *', { name: BATCH_TOP_PROPERTIES })
+	public async batchTopProperties() {
 		try {
 			this.logger['context'] = BATCH_TOP_PROPERTIES;
 			this.logger.debug('EXECUTED!');
-			await this.batchService.batchProperties();
+			await this.batchService.batchTopProperties();
 		} catch (err) {
 			this.logger.error(err);
 		}
 	}
-	@Cron('40 * * * * *', { name: BATCH_TOP_AGENTS })
+	@Cron('40 00 01 * * *', { name: BATCH_TOP_AGENTS })
 	public async batchAgents() {
 		try {
 			this.logger['context'] = BATCH_TOP_AGENTS;
